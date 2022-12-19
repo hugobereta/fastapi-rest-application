e69051181ba644f944d16dd83d63b8726fd1425f
Hugo Bereta <hugo_bereta@yahoo.com.br>
2022-12-18 21:13:39 -0300

    Add some comments at main.py and db.py

---
+++ app/main.py
@@ -0,0 +1,4 @@
+# Building REST APIs using FastAPI, SQLAlchemy & Uvicorn
+# https://medium.com/@dassum/building-rest-apis-using-
+# fastapi-sqlalchemy-uvicorn-8a163ccf3aa1
+# By Suman Das
--- app/models/db.py
+++ app/models/db.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# Creates database in this same directory
 SQL_ALCHEMY_DATABASE_URL = 'sqlite:///./data.db'
 
+# This arg is only for SQLite
 CONNECT_ARGS = {
     'check_same_thread': False,
 }
@@ -14,13 +16,19 @@ engine = create_engine(
     echo=True,
 )
 
-session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# sessionmaker is a factory
+# SessionLocal is a class to represent database session when it is instantiated
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-base = declarative_base()
+# The declarative_base return a class to create Base class.
+# Later, we will inherit from this class to create each
+# of the database models or classes(the ORM models).
+Base = declarative_base()
 
 
+# Creates an independent database session for each request
 def get_db():
-    db = session_local()
+    db = SessionLocal()
     try:
         yield db
     finally:
